2aa535dc646821f6d36ff55a4449b204e3d1e07d
Alan Hape <ahape@brightmetrics.com>
2022-07-24 13:08:41 -0700

    Make more user friendly

--- cstags.py
+++ cstags.py
@@ -7,6 +7,8 @@ rx_class = re.compile(fr"^\s*(?:{r_name_attr}\s+)*class\s+({r_name})")
 rx_interface = re.compile(fr"^\s*(?:{r_name}\s+)*interface\s+({r_name})")
 rx_method = re.compile(fr"^\s*({r_name_attr}\s+)+({r_name}\(.*\))")
 
+files_scanned = 0
+
 def main():
   tags = []
   start_time = time.time()
@@ -20,15 +22,16 @@ def main():
       else:
         print(f"Warning: Skipping file '{file_name}'. Not '.cs' file type")
 
-  print("--- tags start ---")
-  print("\n".join(tags))
-  print("--- tags end ---")
+  if os.getenv("DEBUG"):
+    print("--- tags start ---")
+    print("\n".join(tags))
+    print("--- tags end ---")
 
   with open("tags", "w") as file:
     tags.sort()
     file.write("\n".join(tags))
 
-  print(f"Done in {time.time() - start_time}")
+  print(f"Done. {files_scanned:,} files processed in {time.time() - start_time:.3f} seconds")
 
 def get_tags_for_directory(directory=None):
   if not directory:
@@ -36,7 +39,6 @@ def get_tags_for_directory(directory=None):
   tags = []
   for item in os.listdir(directory):
     abs_path = os.path.join(directory, item)
-    print(abs_path)
     if os.path.isfile(abs_path) and item.endswith(".cs"):
       tags += get_tags_for_file(abs_path)
     elif os.path.isdir(abs_path):
@@ -59,6 +61,10 @@ def parse_method(line):
     return sanitize_capture(match.group(2))
 
 def sanitize_capture(capture):
+  """
+  Since ^] only navigates based on the "word", matches with other chars don't
+  get found. This function trims the tag to just be the name of the symbol
+  """
   if not capture:
     return None
   for symbol in ["<", "("]:
@@ -82,6 +88,7 @@ def add_tag(tag, file_name, line, line_num, tags):
     tags.append("\t".join([tag, file_name, f"call cursor({line_num},{col_num})"]))
 
 def get_tags_for_file(file_name):
+  global files_scanned
   try:
     file = open(file_name, "r")
   except:
@@ -96,6 +103,7 @@ def get_tags_for_file(file_name):
       add_method_tag(file_name, line, line_num, tags)
       add_interface_tag(file_name, line, line_num, tags)
       line_num += 1
+    files_scanned += 1
     return tags
 
 if __name__ == "__main__":
